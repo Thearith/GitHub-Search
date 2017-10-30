2f991f958722c6803f80bc476ddedd3c88a465ce
Sothearith Sreang <sothearith.sreang@accedo.tv>
2017-10-30 22:51:55 +0800

    initializes project

---
+++ GitHub-Search/app/src/main/java/thearith/github/com/github_search/MainActivity.kt
@@ -0,0 +1,12 @@
+package thearith.github.com.github_search
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
